dc299797caccf40f7adafb103316c02ace4d26f4
Aditya Jaiswal <jaiswaladitya844@gmail.com>
2022-06-14 03:36:28 +0530

    Time: 10 ms (10.79%), Space: 43.8 MB (63.78%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,17 +1,12 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
-        
-    
-        int[] res = new int[nums1.length];
-        
+
         for (int i=0; i<nums1.length; i++) {
             int max = -1;
             for (int j=nums2.length-1; j>=0; j--) {
                 if (nums2[j] == nums1[i]) {
                     break;
-                }
-                if (nums1[i] < nums2[j]) {
+                } else if (nums1[i] < nums2[j]) {
                     max = nums2[j];
                 } else if (nums1[i] > nums2[j] && max != -1) {
                     continue;
@@ -20,9 +15,9 @@ class Solution {
                 }
             }
             
-            res[i] = max;
+            nums1[i] = max;
         }
         
-        return res;
+        return nums1;
     }
 }
