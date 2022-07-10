666d28b72a5f9270e337270ebd43b1aff08527c6
Aditya Jaiswal <jaiswaladitya844@gmail.com>
2022-07-11 01:26:32 +0530

    Time: 1 ms (99.62%), Space: 40.6 MB (84.66%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,37 +1,20 @@
 class Solution {
-    private boolean isOpening(char c) {
-        return c == '(' || c ==  '{' || c ==  '[';
-    }
     
-    private boolean isMatching(char open, char close) {
-        return (open == '(' && close == ')') || (open == '[' && close == ']') || (open == '{' && close == '}');
-    }
-    public boolean isValid(String s) {
+    public boolean isValid(String s) {    
+        if(s.length() % 2 != 0) return false;
         
-        if (s.length() % 2 == 1) {
-            return false;
-        }
-        
-        Stack<Character> stack = new Stack<>();
-        
-        for (int i=0; i<s.length(); i++) {
-            if (!stack.isEmpty() && isOpening(s.charAt(i))) {
-                stack.push(s.charAt(i));
-                continue;
-            }
-            if (isOpening(s.charAt(i))) {
-                stack.push(s.charAt(i));
-            }
-            else if (!stack.isEmpty() && isMatching(stack.peek(), s.charAt(i))) {
+        Stack<Character> stack = new Stack();
+        for(char c: s.toCharArray()) {
+            if(c == ')' && !stack.isEmpty() && stack.peek() == '(') {
                 stack.pop();
-            } else {
-                return false;
+            }else if(c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            }else if(c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            }else{
+                stack.push(c);
             }
         }
-    
-        if (!stack.isEmpty()) {
-            return false;
-        }
-        return true;
+        return stack.isEmpty();
     }
 }
