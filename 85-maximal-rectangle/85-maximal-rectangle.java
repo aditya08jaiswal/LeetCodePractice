8a8fda0ac72a8301d3c406def9942812e3c0e58c
Aditya Jaiswal <jaiswaladitya844@gmail.com>
2022-07-20 01:32:16 +0530

    Time: 98 ms (7.75%), Space: 55.7 MB (10.78%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -0,0 +1,152 @@
+class Solution {
+    
+    public int[][] convertCharArrayToInt(char[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        int[][] res = new int[rows][cols];
+        
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                res[i][j] = Integer.parseInt(String.valueOf(matrix[i][j]));
+            }
+        }
+        return res;
+    }
+    public int maximalRectangle(char[][] matrix) {
+        
+        int max = Integer.MIN_VALUE;
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        int[][] mat = convertCharArrayToInt(matrix);
+        // System.out.println(Arrays.toString(mat));
+        
+        int[] a = new int[cols];
+        
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                
+                if (i == 0) {
+                    a[j] = mat[i][j];
+                } else {
+                    if (mat[i][j] == 0) {
+                        a[j] = mat[i][j];
+                    } else {
+                        a[j] = a[j] + mat[i][j];
+                    }
+                }
+            }
+            
+            int mah = largestRectangleArea(a);
+            max = Math.max(max, mah);
+        }
+        
+        return max;
+    }
+    
+    private int[] findNSL(int[] heights, int len) {
+        Stack<Integer> s = new Stack<Integer>();
+        int[] nsl = new int[len];
+        
+        for (int i=0; i<len; i++) {
+            
+            if (s.empty()) {
+                nsl[i] = 1;
+            } else if (!s.empty() && heights[s.peek()] < heights[i]) {
+                nsl[i] = i-s.peek();
+            } else if (!s.empty() && heights[s.peek()] >= heights[i]) {
+                while(!s.empty() && heights[s.peek()] >= heights[i]) {
+                    s.pop();
+                }
+                if (s.empty()) {
+                    nsl[i] = i+1;
+                } else {
+                    nsl[i] = i-s.peek();
+                }
+            }
+            
+            s.push(i);
+        }
+        
+        return nsl;
+    }
+    
+    private int[] findNSR(int[] heights, int len) {
+        Stack<Integer> s = new Stack<Integer>();
+        int[] nsr = new int[len];
+        
+        for (int i=len-1; i>=0; i--) {
+            
+            if (s.empty()) {
+                nsr[i] = -1;
+            } else if (!s.empty() && heights[s.peek()] < heights[i]) {
+                nsr[i] = s.peek();
+            } else if (!s.empty() && heights[s.peek()] >= heights[i]) {
+                while(!s.empty() && heights[s.peek()] >= heights[i]) {
+                    s.pop();
+                }
+                if (s.empty()) {
+                    nsr[i] = -1;
+                } else {
+                    nsr[i] = s.peek();
+                }
+            }
+            
+            s.push(i);
+        }
+        
+        for (int i=0; i<len; i++) {
+            if (nsr[i] < 0) {
+                nsr[i] = len - i;
+            } else {
+                nsr[i] = nsr[i] - i;
+            }
+        }
+        
+        return nsr;
+    }
+    
+    private int[] findWidth(int[] nsl, int[] nsr) {
+        
+        for (int i=0; i<nsl.length; i++) {
+            nsl[i] = nsl[i] + nsr[i] - 1;
+        }
+        return nsl;
+    }
+    
+    private int[] findHeight(int[] width, int[] heights) {
+        
+        for (int i=0; i<width.length; i++) {
+            width[i] = width[i] * heights[i];
+        }
+        return width;
+    }
+    
+     private int findMax(int[] width) {
+        int max = width[0];
+        for (int i=1; i<width.length; i++) {
+            if (max < width[i]) {
+                max = width[i];
+            }
+        }
+        return max;
+    }
+    
+    public int largestRectangleArea(int[] heights) {
+        
+        int len = heights.length;
+        
+        int[] nsl = findNSL(heights, len);
+        int[] nsr = findNSR(heights, len);
+        
+        int[] width = findWidth(nsl, nsr);
+        
+        int[] height = findHeight(width, heights);
+        
+        int max = findMax(height);
+        
+        return max;
+    }
+}
