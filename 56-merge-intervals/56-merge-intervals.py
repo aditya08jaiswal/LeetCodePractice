ef4ac8950d5caa0556ac3c86b591f9427f25ae82
Aditya Jaiswal <jaiswaladitya844@gmail.com>
2022-08-01 23:36:23 +0530

    Time: 349 ms (5.01%), Space: 18.1 MB (51.51%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -7,15 +7,12 @@ class Solution:
         
         i = 1
         while i<len(intervals):
-            res_top = res[len(res)-1]
-            curr = intervals[i]
-            
             # [[1,4],[5,6]]
-            if res_top[1] < curr[0]:
+            if res[len(res)-1][1] < intervals[i][0]:
                 res.append(intervals[i])
             else:
                 # [[1,4],[0,2]] or [[1,4],[3,6]]
-                res[len(res)-1][0] = min(curr[0], res_top[0])
-                res[len(res)-1][1] = max(curr[1], res_top[1])
+                res[len(res)-1][0] = min(intervals[i][0], res[len(res)-1][0])
+                res[len(res)-1][1] = max(intervals[i][1], res[len(res)-1][1])
             i+=1
         return res
