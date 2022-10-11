bfa538911c08266b2396644eddced300a439eed4
Aditya Jaiswal <jaiswaladitya844@gmail.com>
2022-10-11 07:25:53 +0530

    Time: 0 ms (100.00%), Space: 60.3 MB (55.45%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,10 +1,51 @@
 class Solution {
+    
+    public boolean isEven(int n) {
+        if (n%2 == 0) {
+            return true;
+        }
+        return false;
+    }
+    
     public int singleNonDuplicate(int[] nums) {
-        for (int i=0; i<nums.length; i=i+2) {
-            if (i+1 < nums.length && nums[i] != nums[i+1]) {
-                return nums[i];
+
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        int result = -1;
+        
+        
+        if (n == 1) {
+            return nums[0];
+        } 
+        
+        //If the first element is the single element, return it
+        if(nums[0] != nums[1]) return nums[0];
+        
+		//If the last element is the single element, return it
+        if(nums[n - 1] != nums[n - 2]) return nums[n - 1];
+        
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+            
+            if (nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]) {
+                result = nums[mid];
+                break;
+            } else if (nums[mid-1] == nums[mid]) {
+                if (isEven(mid)) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (nums[mid] == nums[mid+1]) {
+                if (isEven(mid)) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
-        return nums[nums.length-1];
+        
+        return result;
     }
 }
