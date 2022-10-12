a8897f7159e1d9fd4efbfa00f69962214b588f9c
Aditya Jaiswal <jaiswaladitya844@gmail.com>
2022-10-13 05:12:02 +0530

    Time: 8 ms (99.70%), Space: 42.5 MB (90.30%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        
+        for (int i=n-1; i>1; i--) {
+            if (nums[i] < nums[i-1] + nums[i-2]) {
+                return nums[i] + nums[i-1] + nums[i-2];
+            }
+        }
+        return 0;
+    }
+}
