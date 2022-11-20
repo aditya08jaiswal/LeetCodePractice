35b408f6ee5445bba90c6ce4ba21014d38046638
Aditya Jaiswal <jaiswaladitya844@gmail.com>
2022-11-21 01:00:26 +0530

    Time: N/A, Memory: N/A',
      },
      questionId: '278',
      submissionId: '847061302',
      sessionId: '2375415',
      getLangDisplay: 'java',
      submissionCode: '/* The isBadVersion API is defined in the parent class VersionControl.\u000A      boolean isBadVersion(int version)\u003B */\u000A\u000Apublic class Solution extends VersionControl {\u000A    public int firstBadVersion(int n) {\u000A        int left \u003D 1\u003B\u000A        int right \u003D n\u003B\u000A        \u000A        int first \u003D 1\u003B\u000A        \u000A        while (left \u003C\u003D right) {\u000A            int mid \u003D left + (right\u002Dleft)/2\u003B\u000A            if (!isBadVersion(mid)) {\u000A                left \u003D mid+1\u003B\u000A            } else {\u000A                first \u003D mid\u003B\u000A                right \u003D mid\u002D1\u003B\u000A            }\u000A        }\u000A        \u000A        return first\u003B\u000A    }\u000A}',
      editCodeUrl: '/explore/item/947',
      checkUrl: '/submissions/detail/847061302/check/',
      runtimeDistributionFormatted: '{\u0022lang\u0022: \u0022java\u0022, \u0022distribution\u0022: [[\u00222\u0022, 0.0047], [\u00223\u0022, 0.0047], [\u00226\u0022, 0.0047], [\u00227\u0022, 0.0047], [\u00228\u0022, 0.014], [\u002215\u0022, 0.7023], [\u002216\u0022, 5.8093], [\u002217\u0022, 8.4279], [\u002218\u0022, 1.6372], [\u002219\u0022, 2.8884], [\u002220\u0022, 4.2558], [\u002221\u0022, 4.1907], [\u002222\u0022, 13.2744], [\u002223\u0022, 8.1023], [\u002224\u0022, 4.6605], [\u002225\u0022, 4.986], [\u002226\u0022, 4.8326], [\u002227\u0022, 3.9442], [\u002228\u0022, 3.293], [\u002229\u0022, 3.1116], [\u002230\u0022, 3.2419], [\u002231\u0022, 3.2837], [\u002232\u0022, 3.0372], [\u002233\u0022, 2.0605], [\u002234\u0022, 1.6884], [\u002235\u0022, 1.2419], [\u002236\u0022, 0.9256], [\u002237\u0022, 0.7674], [\u002238\u0022, 0.8419], [\u002239\u0022, 0.8419], [\u002240\u0022, 0.6698], [\u002241\u0022, 0.6326], [\u002242\u0022, 0.5395], [\u002243\u0022, 0.4558], [\u002244\u0022, 0.4698]]}',
      memoryDistributionFormatted: '{\u0022lang\u0022: \u0022java\u0022, \u0022distribution\u0022: [[\u002233800\u0022, 0.0047], [\u002235300\u0022, 0.0047], [\u002236000\u0022, 0.0047], [\u002237000\u0022, 0.0047], [\u002237600\u0022, 0.0047], [\u002237700\u0022, 0.0047], [\u002238300\u0022, 0.0047], [\u002238400\u0022, 0.0233], [\u002238500\u0022, 0.1349], [\u002238600\u0022, 0.5814], [\u002238700\u0022, 1.8093], [\u002238800\u0022, 2.7395], [\u002238900\u0022, 2.2698], [\u002239000\u0022, 2.9581], [\u002239100\u0022, 3.1907], [\u002239200\u0022, 2.4558], [\u002239300\u0022, 1.0605], [\u002239400\u0022, 0.307], [\u002239500\u0022, 0.093], [\u002239600\u0022, 0.0837], [\u002239700\u0022, 0.1209], [\u002239800\u0022, 0.2837], [\u002239900\u0022, 0.3442], [\u002240000\u0022, 0.6233], [\u002240100\u0022, 0.9628], [\u002240200\u0022, 1.6279], [\u002240300\u0022, 2.1767], [\u002240400\u0022, 3.0512], [\u002240500\u0022, 4.8465], [\u002240600\u0022, 6.3535], [\u002240700\u0022, 7.8047], [\u002240800\u0022, 8.4419], [\u002240900\u0022, 8.307], [\u002241000\u0022, 7.9628], [\u002241100\u0022, 6.9256], [\u002241200\u0022, 5.8372], [\u002241300\u0022, 4.6605], [\u002241400\u0022, 3.7953], [\u002241500\u0022, 2.7395]]}',
      langs: [
        
      ],
      runtime: 'None',
      memory: 'None',
      enableMemoryDistribution: 'True',
      nonSufficientMsg: 'Sorry. We do not have enough accepted submissions to show distribution chart. - LeetHub

---
+++ first-bad-version/first-bad-version.java
@@ -0,0 +1,23 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        
+        int first = 1;
+        
+        while (left <= right) {
+            int mid = left + (right-left)/2;
+            if (!isBadVersion(mid)) {
+                left = mid+1;
+            } else {
+                first = mid;
+                right = mid-1;
+            }
+        }
+        
+        return first;
+    }
+}
