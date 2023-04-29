624ae8dc3b4ecdd2922e9801ceffebea0dfeafde
Aditya Jaiswal <jaiswaladitya844@gmail.com>
2023-04-30 03:19:13 +0530

    Time: 0 ms, Memory: 39.4 MB - LeetHub

---
+++ climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        if (n == 1) return 1;
+
+        int prev = 1, curr = 1;
+
+        for(int i=2; i<=n; i++) {
+            int sum = prev + curr;
+            prev = curr;
+            curr = sum;
+        }
+
+        return curr;
+    }
+}
